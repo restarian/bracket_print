3f1a19487027f3c14d926016877aa5a3249e120e
Robert Steckroth <robertsteckroth@gmail.com>
2017-11-01 21:14:18 -0400

    closer to working design

--- runme.js
+++ runme.js
@@ -9,8 +9,10 @@ requirejs(["./lib/bracket_print.js"], function(print) {
 
 var up = new print("Print is here")
 //print.prototype.platform = "terminal"
-//console.log(up.sp("???????", {cool: "joes"}).sp(true).toStyleString())
-console.log(up.sp("???????", {cool: "joes"}).option({style: false}).sp(true, null).option({style: true}).toStyleString())
-//up.option({level: 2}).sp("???????", {cool: "joes"}).log(22233, "*****")
+//console.log(up.sp("???????", {cool: "joes"}).sp(true, null).toStyleString())
+//console.log(up.sp("???????", {cool: "joes"}).option({style: false}).sp(true, null).option({style: true}).toStyleString())
+up.option({level: 2}).sp("???????", {cool: "joes"}).log(22233, "*****", null, true)
+//up.empty().option({level: 1}).sp("???????", {cool: "joes"}).log(22233, "*****", null, true)
+
 })
 
