13e9fc3199ac5e19170db46d9462bb434a849f54
Robert Steckroth <RobertSteckroth@gmail.com>
2017-12-16 08:14:05 -0800

    Removed superfluous license texts

--- build/bracket_print.js
+++ build/bracket_print.js
@@ -1,21 +1,3 @@
-/* Bracket Print resides under the LGPL v3
-
-  Brackit print is a printing and logging tool for javascript engines which supplies literal ECMA Object serialization.
-
-  Copyright (C) 2017  Robert Edward Steckroth II <RobertSteckroth@gmail.com>
-
- this file is a part of Brackit print
-
- Brackit Print is free software: you can redistribute it and/or modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
- the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-
- Brackit print is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-  Author: Robert Edward Steckroth, Bustout, <RobertSteckroth@gmail.com> */
-
 /* Brace Prototype resides under the MIT license
 
 Copyright (c) 2017 Robert Edward Steckroth <RobertSteckroth@gmail.com>
@@ -43,31 +25,13 @@ SOFTWARE.
  this file is part of the Brace Prototype module
  Author: Robert Edward Steckroth II, Bustout, <RobertSteckroth@gmail.com> */
 
-/*Bracket Print resides under the LGPL v3
-
-  Brackit print is a printing and logging tool for javascript engines which supplies literal ECMA Object serialization.
-
-  Copyright (C) 2017  Robert Edward Steckroth II <RobertSteckroth@gmail.com>
-
- this file is a part of Brackit print
-
- Bracket Print is free software: you can redistribute it and/or modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
- the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-
- Bracket print is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-  Author: Robert Edward Steckroth, Bustout, <RobertSteckroth@gmail.com> */
-
 /* Bracket Print resides under the LGPL v3
 
   Brackit print is a printing and logging tool for javascript engines which supplies literal ECMA Object serialization.
 
   Copyright (C) 2017  Robert Edward Steckroth II <RobertSteckroth@gmail.com>
 
- this file is a part of Bracket print
+ this file and all files in this directory are a part of Bracket print
 
  Bracket Print is free software: you can redistribute it and/or modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
  the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
